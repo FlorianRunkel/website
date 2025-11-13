28363a536a051b21e7f988fc789a0482d2a22132
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2025-11-13 11:00:19 +0100

    update website

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
             }}
             onClick={() => handleNavClick('me')}
           >
-            PORTFOLIO.
+            Florian Runkel
           </Typography>
 
           {/* Navigation für große Screens (md und größer) */}
